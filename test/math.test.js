fc60eb6f18969e1fd93f0781dcb185ea5d63e304
Venus-Aggarwal1 <venu2ka@gmail.com>
2026-02-15 18:58:32 +0530

    added test directory

---
+++ test/math.test.js
@@ -0,0 +1,22 @@
+// math.test.js
+const { sum, multiply } = require('./math');
+
+// 'describe' groups related test cases together (a test suite)
+describe('Math functions', () => {
+
+  // 'test' (or 'it') defines a single test case
+  test('adds 1 + 2 to equal 3', () => {
+    // 'expect' takes the actual result, followed by a 'matcher' function
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test('multiplies 3 * 4 to equal 12', () => {
+    expect(multiply(3, 4)).toBe(12);
+  });
+
+  test('throws an error if arguments are invalid', () => {
+    // To test if a function throws an error, you must wrap the function call in another function
+    expect(() => sum('a', 2)).toThrow();
+  });
+});
+
